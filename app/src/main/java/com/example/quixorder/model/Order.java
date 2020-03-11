5be97be07da097cdcaee88b535f3efed37952da2
EatThisShoe <eric.alexander.lawson@gmail.com>
2020-03-10 22:06:27 -0700

    Added Order class. Using DocumentReference for firebase references, Still need to convert it into MenuItem objects manually.

--- app/src/main/java/com/example/quixorder/model/Order.java
+++ app/src/main/java/com/example/quixorder/model/Order.java
@@ -1,5 +1,42 @@
 package com.example.quixorder.model;
 
+import com.google.firebase.firestore.DocumentReference;
+
+import java.util.Date;
+
 public class Order {
-    //TODO
+    private String table;
+    private Date startTime;
+    private Date cookedTime;
+    private Date servedTime;
+    private DocumentReference[] orderItems;
+    private String server;
+
+    public String getTable() {
+        return table;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getCookedTime() {
+        return cookedTime;
+    }
+
+    public Date getServedTime() {
+        return servedTime;
+    }
+
+    public DocumentReference[] getOrderItems() {
+        return orderItems;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public Order() {
+
+    }
 }
