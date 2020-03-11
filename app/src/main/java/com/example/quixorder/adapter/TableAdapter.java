e2563b11c92a0c906d2b8a80cd88d5303e7aa318
John Ta <john.ta@student.csulb.edu>
2020-03-11 16:59:08 -0700

    Made working concepts of recycler and card views

--- app/src/main/java/com/example/quixorder/adapter/TableAdapter.java
+++ app/src/main/java/com/example/quixorder/adapter/TableAdapter.java
@@ -1,6 +1,57 @@
 package com.example.quixorder.adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class TableAdapter extends RecyclerView.Adapter {
+import com.example.quixorder.R;
+import com.example.quixorder.item.TableItem;
+
+import java.util.ArrayList;
+
+public class TableAdapter extends RecyclerView.Adapter<TableAdapter.ExampleViewHolder> {
+    private ArrayList<TableItem> tableList;
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView1;
+        public TextView textView2;
+
+        public ExampleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView1 = itemView.findViewById(R.id.textView1);
+            textView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public TableAdapter(ArrayList<TableItem> tableList) {
+        this.tableList = tableList;
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.table_item, viewGroup, false);
+        ExampleViewHolder evh = new ExampleViewHolder(view);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder exampleViewHolder, int i) {
+        TableItem currentItem = tableList.get(i);
+
+        exampleViewHolder.imageView.setImageResource(currentItem.getImageResource());
+        exampleViewHolder.textView1.setText(currentItem.getText1());
+        exampleViewHolder.textView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tableList.size();
+    }
 }
--- app/src/main/java/com/example/quixorder/fragment/AssignTableFragment.java
+++ app/src/main/java/com/example/quixorder/fragment/AssignTableFragment.java
@@ -4,17 +4,51 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.quixorder.R;
+import com.example.quixorder.adapter.TableAdapter;
+import com.example.quixorder.item.TableItem;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class AssignTableFragment extends Fragment {
+    private FirebaseFirestore firebase = FirebaseFirestore.getInstance();
+
+    private View view;
+    private RecyclerView tableView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_assign_table, container, false);
+        view = inflater.inflate(R.layout.fragment_assign_table, container, false);
+
+        ArrayList<TableItem> tableList = new ArrayList<>();
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 1", "Line 2"));
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 3", "Line 4"));
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 5", "Line 6"));
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 7", "Line 8"));
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 9", "Line 10"));
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 11", "Line 12"));
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 13", "Line 14"));
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 15", "Line 16"));
+        tableList.add(new TableItem(R.drawable.ic_running_order, "Line 17", "Line 18"));
+
+        tableView = view.findViewById(R.id.tablesView);
+        tableView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getContext());
+        adapter = new TableAdapter(tableList);
+
+        tableView.setLayoutManager(layoutManager);
+        tableView.setAdapter(adapter);
+
+        return view;
     }
 }
