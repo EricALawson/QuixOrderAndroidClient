9b9a8ba88abe7a7644857da2af33dd36e6015c99
EatThisShoe <eric.alexander.lawson@gmail.com>
2020-03-13 18:24:53 -0700

    Added a constructor to the Order class which builds the MenuItem objects from the DocumentReferences from firebase.

--- app/src/main/java/com/example/quixorder/model/Order.java
+++ app/src/main/java/com/example/quixorder/model/Order.java
@@ -1,6 +1,9 @@
 package com.example.quixorder.model;
 
+import android.util.Log;
+
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.util.Date;
 
@@ -10,7 +13,32 @@ public class Order {
     private Date cookedTime;
     private Date servedTime;
     private DocumentReference[] orderItems;
+    private MenuItem[] orderMenuItems;
     private String server;
+
+    public Order() {
+
+    }
+
+    public Order(DocumentReference orderDocument) {
+        DocumentSnapshot snapshot = orderDocument.get().getResult();
+        if(snapshot != null && snapshot.exists()) {
+            table = (String) snapshot.get("table");
+            server = (String) snapshot.get("server");
+            startTime = (Date) snapshot.get("startTime");
+            cookedTime = (Date) snapshot.get("cookedTime");
+            servedTime = (Date) snapshot.get("servedTime");
+            orderItems = (DocumentReference[]) snapshot.get("orderItems");
+            
+            orderMenuItems = new MenuItem[orderItems.length];
+            for (int orderItemCount = 0; orderItemCount < orderItems.length; orderItemCount++) {
+                orderMenuItems[orderItemCount] = orderItems[orderItemCount].get().getResult().toObject(MenuItem.class);
+            }
+
+        } else {
+            Log.e("Bad DocumentReference", "Can't create Order object from document reference in constructor");
+        }
+    }
 
     public String getTable() {
         return table;
@@ -36,7 +64,7 @@ public class Order {
         return server;
     }
 
-    public Order() {
-
+    public MenuItem[] getOrderMenuItems() {
+        return orderMenuItems;
     }
 }
