4bfdae1edc1b6d80d5febaa7c570860eadfe8deb
John Ta <john.ta@student.csulb.edu>
2020-03-31 20:25:03 -0700

    Updated remove item type
    Implemented interface, and switch case dependent on the view selected

--- app/src/main/java/com/example/quixorder/adapter/ItemTypeAdapter.java
+++ app/src/main/java/com/example/quixorder/adapter/ItemTypeAdapter.java
@@ -1,9 +1,9 @@
 package com.example.quixorder.adapter;
 
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.quixorder.R;
@@ -19,12 +19,14 @@ import androidx.recyclerview.widget.RecyclerView;
 public class ItemTypeAdapter extends RecyclerView.Adapter<ItemTypeAdapter.ViewHolder> {
     private ArrayList<ItemType> itemTypeList;
     private OnItemTypeListener onItemTypeListener;
+    private OnRemoveItemTypeListener onRemoveItemTypeListener;
     private int selectedItem = RecyclerView.NO_POSITION;
     private int previousItem = RecyclerView.NO_POSITION;
 
-    public ItemTypeAdapter(ArrayList<ItemType> itemTypeList, OnItemTypeListener onItemTypeListener) {
+    public ItemTypeAdapter(ArrayList<ItemType> itemTypeList, OnItemTypeListener onItemTypeListener, OnRemoveItemTypeListener onRemoveItemTypeListener) {
         this.itemTypeList = itemTypeList;
         this.onItemTypeListener = onItemTypeListener;
+        this.onRemoveItemTypeListener = onRemoveItemTypeListener;
     }
 
     @Override
@@ -36,7 +38,7 @@ public class ItemTypeAdapter extends RecyclerView.Adapter<ItemTypeAdapter.ViewHo
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_type_item, viewGroup, false);
-        ViewHolder evh = new ViewHolder(view, onItemTypeListener);
+        ViewHolder evh = new ViewHolder(view, onItemTypeListener, onRemoveItemTypeListener);
         return evh;
     }
 
@@ -44,51 +46,74 @@ public class ItemTypeAdapter extends RecyclerView.Adapter<ItemTypeAdapter.ViewHo
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
         ItemType currentItem = itemTypeList.get(i);
 
-        // Set views
+        // Set colors for when clicked
         int backgroundColor;
         int fontColor;
+        int removeColor;
         if (i == selectedItem) {
             backgroundColor = R.color.itemTypeSelected;
             fontColor = R.color.itemTypeSelectedFont;
+            removeColor = R.drawable.ic_remove_circle_wht;
         } else {
             backgroundColor = (i % 2 == 0) ? R.color.itemTypeUnselected1 : R.color.itemTypeUnselected2;
             fontColor = R.color.itemTypeUnselectedFont;
+            removeColor = R.drawable.ic_remove_circle_blk;
         }
 
+        // Set views
         viewHolder.cardView.setBackgroundColor(ContextCompat.getColor(viewHolder.cardView.getContext(), backgroundColor));
         viewHolder.textView1.setTextColor(ContextCompat.getColor(viewHolder.textView1.getContext(), fontColor));
         viewHolder.textView1.setText(currentItem.getType());
+        viewHolder.imageView.setImageResource(removeColor);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private CardView cardView;
         private TextView textView1;
+        private ImageView imageView;
         private OnItemTypeListener onItemTypeListener;
+        private OnRemoveItemTypeListener onRemoveItemTypeListener;
 
-        public ViewHolder(@NonNull View itemView, OnItemTypeListener onItemTypeListener) {
+        public ViewHolder(@NonNull View itemView, OnItemTypeListener onItemTypeListener, OnRemoveItemTypeListener onRemoveItemTypeListener) {
             super(itemView);
 
             cardView = itemView.findViewById(R.id.cardView);
             textView1 = itemView.findViewById(R.id.textView1);
+            imageView = itemView.findViewById(R.id.imageView);
 
-            // Load item type click listener
+            // Load click listeners
             this.onItemTypeListener = onItemTypeListener;
-            itemView.setOnClickListener(this);
+            this.onRemoveItemTypeListener = onRemoveItemTypeListener;
+            cardView.setOnClickListener(this);
+            imageView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            // Update items
-            previousItem = selectedItem;
-            selectedItem = getAdapterPosition();
-            notifyItemChanged(previousItem);
-            notifyItemChanged(selectedItem);
+            switch(view.getId()) {
+                case R.id.cardView:
+                    // Update items
+                    previousItem = selectedItem;
+                    selectedItem = getAdapterPosition();
+                    notifyItemChanged(previousItem);
+                    notifyItemChanged(selectedItem);
 
-            onItemTypeListener.onItemTypeClick(getAdapterPosition(), itemTypeList.get(getAdapterPosition()).getType());
+                    onItemTypeListener.onItemTypeClick(getAdapterPosition(), itemTypeList.get(getAdapterPosition()).getType());
+                    break;
+
+                case R.id.imageView:
+                    // Update remove item
+                    onRemoveItemTypeListener.onRemoveItemTypeListener(getAdapterPosition(), itemTypeList.get(getAdapterPosition()).getType());
+            }
+
         }
     }
 
     public interface OnItemTypeListener {
         void onItemTypeClick(int position, String itemType);
+    }
+
+    public interface OnRemoveItemTypeListener {
+        void onRemoveItemTypeListener(int position, String itemType);
     }
 }
