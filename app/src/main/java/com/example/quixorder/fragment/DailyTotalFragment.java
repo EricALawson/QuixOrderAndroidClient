75f97b6731aa90a4c93272053dcefd73ed45b894
John Ta <john.ta@student.csulb.edu>
2020-04-04 22:17:47 -0700

    Updated date frequency filter

--- app/src/main/java/com/example/quixorder/fragment/DailyTotalFragment.java
+++ app/src/main/java/com/example/quixorder/fragment/DailyTotalFragment.java
@@ -7,14 +7,44 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.quixorder.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class DailyTotalFragment extends Fragment {
+public class DailyTotalFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    private FirebaseFirestore firebase = FirebaseFirestore.getInstance();
+
+    private View view;
+    private Spinner frequencySpinner;
+    private ArrayAdapter<CharSequence> frequencyAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_daily_total, container, false);
+        view = inflater.inflate(R.layout.fragment_daily_total, container, false);
+
+        // Set up views
+        frequencySpinner = view.findViewById(R.id.frequencySpinner);
+
+        // Populate views
+        frequencyAdapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.frequencies, android.R.layout.simple_spinner_item);
+        frequencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        frequencySpinner.setAdapter(frequencyAdapter);
+        frequencySpinner.setOnItemSelectedListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
